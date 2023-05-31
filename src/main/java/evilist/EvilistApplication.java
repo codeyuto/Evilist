66c8f95a9f2b3b48549eac0a5fee2b4431ec3957
nantai_yuto <takosuke29@gmail.com>
2023-05-31 22:08:34 +0900

    Made Controller,Application,index.html

---
+++ src/main/java/evilist/EvilistApplication.java
@@ -0,0 +1,18 @@
+package evilist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class EvilistApplication {
+
+		public static void main(String[] args) {
+			SpringApplication.run(EvilistApplication.class, args);
+		}
+
+	}
+
+
+
