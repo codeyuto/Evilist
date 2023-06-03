45ec534535d61e03a6e0cf3c6a15c1bb1bcceb93
nantai_yuto <takosuke29@gmail.com>
2023-06-03 20:59:39 +0900

    いつくかページを追加

--- src/main/java/evilist/EvilistController.java
+++ src/main/java/evilist/EvilistController.java
@@ -3,6 +3,7 @@ package evilist;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class EvilistController {
@@ -12,6 +13,40 @@ public class EvilistController {
 		return "index";
 		
 	}
+	@GetMapping("/login")
+	public String login(Model model) {
+		
+		return "login";
+	}
+	
+	
+	@PostMapping("/auth")
+	public String auth(Model model){
+		System.out.print("000");
+		
+		
+		return "redirect:/userpage";
+		
+	}
+	
+	@GetMapping("/userpage")
+	public String userpage(Model model) {
+		
+		return "userpage";
+	}
+	
+	@GetMapping("/textform")
+	public String textform(Model model) {
+		return "textform";
+	}
+	
+	@PostMapping("/formsubmit")
+	public String formsubmit(Model model) {
+		
+		return "result";
+	}
+	
+	
 	
 
 }
